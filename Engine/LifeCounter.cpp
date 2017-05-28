de81d6de77481d93ef59f529d11478e8cdc7c312
Kevin Lackó <kevinlacko.kl@gmail.com>
2017-05-28 14:54:50 +0200

    prevened future bug, when power ups would be implemented

--- Engine/LifeCounter.cpp
+++ Engine/LifeCounter.cpp
@@ -17,7 +17,7 @@ LifeCounter::LifeCounter(Vec2 pos_in, int lives_in)
 // returns true if u have no life
 bool LifeCounter::ConsumeLife()
 {
-	lives--;
+	lives = max(lives--,0);
 	if (lives > 0 && lives < 10)
 	{
 		pads.pop_back();
